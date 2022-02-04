4b45e06d56f819ba2b2dd311fcfff9d969fc853f
Gustavo Corchog <gugacorchog@gmail.com>
2022-02-04 22:07:11 +1100

    first commit

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,10 @@ if ( process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, "./frontend/build")))
 }
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+})
+
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
